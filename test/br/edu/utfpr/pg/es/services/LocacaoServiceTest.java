26e7aa57fa253d3a4977aaad355742bb77d301e9
vinic <vinic@10.20.6.219>
2019-06-07 20:08:45 -0300

    v.1.0.1 - add calcularValorLocacaoTresFilmesTest()

--- test/br/edu/utfpr/pg/es/services/LocacaoServiceTest.java
+++ test/br/edu/utfpr/pg/es/services/LocacaoServiceTest.java
@@ -34,4 +34,28 @@ public class LocacaoServiceTest {
         // ======================= VERIFICAÇÃO ======================== //
         assertThat(valorLocacao, is(8.00));
     }
+    
+    @Test
+    public void calcularValorLocacaoTresFilmesTest() {
+
+        // ==================== MONTAGEM DO CENÁRIO ==================== //
+        Cliente cliente = new Cliente(123, "João da Silva");
+        Filme filme1 = new Filme(1, "Vingadores Ultimato", "Ação", 4.00);
+        Filme filme2 = new Filme(2, "X-Men: Fênix Negra", "Ação", 4.00);
+        Filme filme3 = new Filme(3, "O Rei Leão", "Animação", 4.00);
+        
+        List<Filme> filmes = new ArrayList<>();
+        filmes.add(filme1);
+        filmes.add(filme2);
+        filmes.add(filme3);
+
+        LocacaoService locacaoService = new LocacaoService();
+        
+        // ========================= EXECUÇÃO ========================= //
+        double valorLocacao = locacaoService.calcularValorLocacao(filmes);
+
+
+        // ======================= VERIFICAÇÃO ======================== //
+        assertThat(valorLocacao, is(11.00));
+    }
 }
